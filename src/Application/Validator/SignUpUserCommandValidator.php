c6b675c83b630368566bd3dc79e73cb6e0d60262
Darnell <collinsdarnell44@gmail.com>
2019-07-02 21:02:45 -0400

    trying something else out

--- src/Application/Validator/SignUpUserCommandValidator.php
+++ src/Application/Validator/SignUpUserCommandValidator.php
@@ -7,15 +7,18 @@ use Tailgate\Application\Validator\User\UniqueEmail;
 use Tailgate\Application\Validator\User\UniqueUsername;
 use Tailgate\Application\Validator\User\UniqueEmailException;
 use Tailgate\Application\Validator\User\UniqueUsernameException;
+use Tailgate\Domain\Model\User\UserViewRepositoryInterface;
 
 class SignUpUserCommandValidator
 {
     protected $rules = [];
     protected $messages = [];
     protected $errors = [];
+    private $userViewRepository;
 
-    public function __construct()
+    public function __construct(UserViewRepositoryInterface $userViewRepository)
     {
+        $this->userViewRepository = $userViewRepository;
         $this->initRules();
         $this->initMessages();
     }
@@ -24,7 +27,7 @@ class SignUpUserCommandValidator
     {
         v::with("Tailgate\Application\Validator\User\\");
 
-        $this->rules['username'] = V::stringType()->noWhitespace()->length(5, null)->UniqueEmail()->UniqueUsername();
+        $this->rules['username'] = V::stringType()->noWhitespace()->length(5, null)->UniqueEmail($this->userViewRepository)->UniqueUsername($this->userViewRepository);
         $this->rules['password'] = V::stringType()->length(5, null);
         $this->rules['email'] = V::email();
     }
