fa3fd8c22d95ff336d984fcbc4cac53c5fe64410
Darnell <collinsdarnell44@gmail.com>
2019-08-18 14:57:02 -0400

    typo

--- src/Infrastructure/Persistence/Event/SeasonProjectorEventSubscriber.php
+++ src/Infrastructure/Persistence/Event/SeasonProjectorEventSubscriber.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tailgate\Common\Event;
+namespace Tailgate\Infrastructure\Persistence\Event;
 
 use Buttercup\Protects\DomainEvent;
 use Tailgate\Common\Event\EventSubscriberInterface;
