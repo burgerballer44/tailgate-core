33e9a21998f46cd5acc664738fad3b8a48d32f1a
Darnell <collinsdarnell44@gmail.com>
2019-09-24 17:59:10 -0400

    typo

--- src/Application/DataTransformer/TeamViewArrayDataTransformer.php
+++ src/Application/DataTransformer/TeamViewArrayDataTransformer.php
@@ -4,15 +4,20 @@ namespace Tailgate\Application\DataTransformer;
 
 use Tailgate\Application\DataTransformer\TeamDataTransformerInterface;
 use Tailgate\Application\DataTransformer\FollowDataTransformerInterface;
+use Tailgate\Application\DataTransformer\GameDataTransformerInterface;
 use Tailgate\Domain\Model\Team\TeamView;
 
 class TeamViewArrayDataTransformer implements TeamDataTransformerInterface
 {
     private $followViewTransformer;
+    private $gameViewTransformer;
 
-    public function __construct(FollowDataTransformerInterface $followViewTransformer)
-    {
+    public function __construct(
+        FollowDataTransformerInterface $followViewTransformer,
+        GameDataTransformerInterface $gameViewTransformer
+    ) {
         $this->followViewTransformer = $followViewTransformer;
+        $this->gameViewTransformer = $gameViewTransformer;
     }
 
     public function read(TeamView $teamView)
